3b10e3069c0d99067e6b2f925e84755c2d09599e
Chhay Ham <chhayham@gmail.com>
2026-05-05 18:43:58 -0700

    added console output updated values updated keybind

--- micro/keyboard-thumbstick.ino
+++ micro/keyboard-thumbstick.ino
@@ -6,11 +6,13 @@ const int PIN_Y = A1;
 const int PIN_B = 14; // sel pin
 
 // Thresholds for directional movement
-const int threshold = 300; 
+const int threshold = 300;
 const int center = 512;
 
+
 void setup() {
-  pinMode(PIN_B, INPUT);
+  pinMode(PIN_B, INPUT_PULLUP);
+  Serial.begin(9600);
   Keyboard.begin();
 }
 
@@ -18,32 +20,42 @@ void loop() {
   int xValue = analogRead(PIN_X);
   int yValue = analogRead(PIN_Y);
   int bValue = digitalRead(PIN_B);
+  bool isPressed = (bValue == LOW);
 
   // Up/Down (Y-axis)
   if (yValue < center - threshold) {
-    Keyboard.press('KEY_UP_ARROW'); // Up
+    Serial.print("up value: ");
+    Serial.println(yValue);
+    Keyboard.press(KEY_UP_ARROW); // Up key
   } else if (yValue > center + threshold) {
-    Keyboard.press('KEY_DOWN_ARROW'); // Down
+    Serial.print("down value: ");
+    Serial.println(yValue);
+    Keyboard.press(KEY_DOWN_ARROW); // Down key
   } else {
-    Keyboard.release('KEY_UP_ARROW');
-    Keyboard.release('KEY_DOWN_ARROW');
+    Keyboard.release(KEY_UP_ARROW);
+    Keyboard.release(KEY_DOWN_ARROW);
   }
 
   // Left/Right (X-axis)
   if (xValue < center - threshold) {
-    Keyboard.press('KEY_LEFT_ARROW'); // Left
+    Serial.print("left value: ");
+    Serial.println(xValue);
+    Keyboard.press(KEY_LEFT_ARROW); // Left key
   } else if (xValue > center + threshold) {
-    Keyboard.press('KEY_RIGHT_ARROW'); // Right
+    Serial.print("right value: ");
+    Serial.println(xValue);
+    Keyboard.press(KEY_RIGHT_ARROW); // Right key
   } else {
-    Keyboard.release('KEY_LEFT_ARROW');
-    Keyboard.release('KEY_RIGHT_ARROW');
+    Keyboard.release(KEY_LEFT_ARROW);
+    Keyboard.release(KEY_RIGHT_ARROW);
   }
 
   // Button
-  if (bValue == HIGH) {
-    Keyboard.press('`'); // ` keybind
+  if (isPressed) {
+    Serial.println("button pressed");
+    Keyboard.press('\\'); // '\\' key, rebind to any key
   } else {
-    Keyboard.release('`');
+    Keyboard.release('\\');
   }
   
   delay(50); // Small delay for stability
